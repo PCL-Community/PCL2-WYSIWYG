b58830f63d2400a21b10fba43a05c2c3ab010a9e
Silverteal <67404722+Silverteal@users.noreply.github.com>
2025-01-25 23:06:43 +0800

    移动注册表目录的定义位置

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -13,6 +13,7 @@ Public Module ModBase
 
     '下列信息为WYSIWYG版新增
     Public Const UpdateRepository As String = "PCL-Community/PCL2-WYSIWYG"
+    Public Const RegistryRootPath As String = "PCL-WYSIWYG"
 
     '下列版本信息由更新器自动修改
     Public Const VersionBaseName As String = "2.8.13" '不含分支前缀的显示用版本名
